e4a186f9ea4aa61d207c23e584ed87dc25407112
pranava <pranava@ibuprofen.shef.ac.uk>
2016-11-16 15:17:49 +0000

    adding stuff

--- sample-de/download_files.sh
+++ sample-de/download_files.sh
@@ -7,16 +7,19 @@ then
   wget http://www.statmt.org/europarl/v7/de-en.tgz -O data/de-en.tgz
 fi
 
-if [ ! -f data/SETIMES2.de-en.txt.zip ];
-then
-  wget http://opus.lingfil.uu.se/download.php?f=SETIMES2/en-de.txt.zip -O data/SETIMES2.de-en.txt.zip
-fi
+#if [ ! -f data/SETIMES2.de-en.txt.zip ];
+#then
+#  wget http://opus.lingfil.uu.se/download.php?f=SETIMES2/en-de.txt.zip -O data/SETIMES2.de-en.txt.zip
+#fi
 
 cd data/
 tar -xf de-en.tgz
-unzip SETIMES2.de-en.txt.zip
+#unzip SETIMES2.de-en.txt.zip
 
-cat europarl-v7.de-en.en SETIMES2.en-de.en > corpus.en
-cat europarl-v7.de-en.de SETIMES2.en-de.de > corpus.de
+#cat europarl-v7.de-en.en SETIMES2.en-de.en > corpus.en
+#cat europarl-v7.de-en.de SETIMES2.en-de.de > corpus.de
+
+#cat europarl-v7.de-en.en > corpus.en
+#cat europarl-v7.de-en.de > corpus.de
 
 cd ..
